500ac005bd7ec4294b1599ee0765a3ccfbaf86ab
Vinukshen <vinukshen@gmail.com>
2020-11-29 14:01:37 +0000

    JSON parsing successful

--- app/src/main/java/com/vinu/chucknorris/QueryUtils.kt
+++ app/src/main/java/com/vinu/chucknorris/QueryUtils.kt
@@ -1,185 +1,152 @@
-package com.vinu.chucknorris;
+package com.vinu.chucknorris
 
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.text.TextUtils;
-import android.util.Log;
+import android.text.TextUtils
+import android.util.Log
+import org.json.JSONException
+import org.json.JSONObject
+import java.io.BufferedReader
+import java.io.IOException
+import java.io.InputStream
+import java.io.InputStreamReader
+import java.net.HttpURLConnection
+import java.net.MalformedURLException
+import java.net.URL
+import java.nio.charset.Charset
+import java.util.*
 
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.nio.charset.Charset;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.regex.Pattern;
-
-public class QueryUtils {
-
-
-    private QueryUtils() {
-    }
-
-    /** Tag for the log messages */
-    public static final String LOG_TAG = QueryUtils.class.getSimpleName();
-
+object QueryUtils {
+    /** Tag for the log messages  */
+    val LOG_TAG = QueryUtils::class.java.simpleName
 
     /**
-     * Query the USGS dataset and return a list of {@link JokeObject} objects.
+     * Query the USGS dataset and return a list of [JokeObject] objects.
      */
-
-    public static ArrayList<String> fetchData(String requestUrl) {
-
-        //Below is the code to sleep. Used to check the ProgressBar!
-//        try {
-//            Thread.sleep(2000);
-//        } catch (InterruptedException e) {
-//            e.printStackTrace();
-//        }
+    fun fetchData(requestUrl: String): ArrayList<String>? {
 
         // Create URL object
-        URL url = createUrl(requestUrl);
+        val url = createUrl(requestUrl)
 
         // Perform HTTP request to the URL and receive a JSON response back
-        String jsonResponse = null;
+        var jsonResponse: String? = null
         try {
-            jsonResponse = makeHttpRequest(url);
-        } catch (IOException e) {
-            Log.e(LOG_TAG, "Problem making the HTTP request.", e);
+            jsonResponse = makeHttpRequest(url)
+        } catch (e: IOException) {
+            Log.e(LOG_TAG, "Problem making the HTTP request.", e)
         }
 
         // Extract relevant fields from the JSON response and create a list of {@link Joke}s
-        ArrayList<String> jokes = extractFeatureFromJson(jsonResponse);
 
         // Return the list of {@link Joke}s
-        return jokes;
+        return extractFeatureFromJson(jsonResponse)
     }
-
-
-
-    //    CODE BELOW IS NEEDED TO EXECUTE THE NETWORK REQUEST. NO NEED TO AMEND IT!!
-
-
 
     /**
      * Returns new URL object from the given string URL.
      */
-    private static URL createUrl(String stringUrl) {
-        URL url = null;
+    private fun createUrl(stringUrl: String): URL? {
+        var url: URL? = null
         try {
-            url = new URL(stringUrl);
-        } catch (MalformedURLException e) {
-            Log.e(LOG_TAG, "Error with creating URL ", e);
+            url = URL(stringUrl)
+        } catch (e: MalformedURLException) {
+            Log.e(LOG_TAG, "Error with creating URL ", e)
         }
-        return url;
+        return url
     }
 
     /**
      * Make an HTTP request to the given URL and return a String as the response.
      */
-    private static String makeHttpRequest(URL url) throws IOException {
-        String jsonResponse = "";
+    @Throws(IOException::class)
+    private fun makeHttpRequest(url: URL?): String {
+        var jsonResponse = ""
 
         // If the URL is null, then return early.
         if (url == null) {
-            return jsonResponse;
+            return jsonResponse
         }
-
-        HttpURLConnection urlConnection = null;
-        InputStream inputStream = null;
+        var urlConnection: HttpURLConnection? = null
+        var inputStream: InputStream? = null
         try {
-            urlConnection = (HttpURLConnection) url.openConnection();
-            urlConnection.setReadTimeout(10000 /* milliseconds */);
-            urlConnection.setConnectTimeout(15000 /* milliseconds */);
-            urlConnection.setRequestMethod("GET");
-            urlConnection.connect();
+            urlConnection = url.openConnection() as HttpURLConnection
+            urlConnection.readTimeout = 10000
+            urlConnection!!.connectTimeout = 15000
+            urlConnection.requestMethod = "GET"
+            urlConnection.connect()
 
             // If the request was successful (response code 200),
             // then read the input stream and parse the response.
-            if (urlConnection.getResponseCode() == 200) {
-                inputStream = urlConnection.getInputStream();
-                jsonResponse = readFromStream(inputStream);
+            if (urlConnection.responseCode == 200) {
+                inputStream = urlConnection.inputStream
+                jsonResponse = readFromStream(inputStream)
             } else {
-                Log.e(LOG_TAG, "Error response code: " + urlConnection.getResponseCode());
+                Log.e(LOG_TAG, "Error response code: " + urlConnection.responseCode)
             }
-        } catch (IOException e) {
-            Log.e(LOG_TAG, "Problem retrieving the joke JSON results.", e);
+        } catch (e: IOException) {
+            Log.e(LOG_TAG, "Problem retrieving the joke JSON results.", e)
         } finally {
-            if (urlConnection != null) {
-                urlConnection.disconnect();
-            }
-            if (inputStream != null) {
-                inputStream.close();
-            }
+            urlConnection?.disconnect()
+            inputStream?.close()
         }
-        return jsonResponse;
+        return jsonResponse
     }
 
     /**
-     * Convert the {@link InputStream} into a String which contains the
+     * Convert the [InputStream] into a String which contains the
      * whole JSON response from the server.
      */
-    private static String readFromStream(InputStream inputStream) throws IOException {
-        StringBuilder output = new StringBuilder();
+    @Throws(IOException::class)
+    private fun readFromStream(inputStream: InputStream?): String {
+        val output = StringBuilder()
         if (inputStream != null) {
-            InputStreamReader inputStreamReader = new InputStreamReader(inputStream, Charset.forName("UTF-8"));
-            BufferedReader reader = new BufferedReader(inputStreamReader);
-            String line = reader.readLine();
+            val inputStreamReader = InputStreamReader(inputStream, Charset.forName("UTF-8"))
+            val reader = BufferedReader(inputStreamReader)
+            var line = reader.readLine()
             while (line != null) {
-                output.append(line);
-                line = reader.readLine();
+                output.append(line)
+                line = reader.readLine()
             }
         }
-        return output.toString();
+        return output.toString()
     }
 
     /**
-     * Return a list of {@link JokeObject} objects that has been built up from
+     * Return a list of [JokeObject] objects that has been built up from
      * parsing the given JSON response.
      */
-    public static ArrayList<String> extractFeatureFromJson(String jokeJSON) {
+    fun extractFeatureFromJson(jokeJSON: String?): ArrayList<String>? {
 
         // If the JSON string is empty or null, then return early.
         if (TextUtils.isEmpty(jokeJSON)) {
-            return null;
+            return null
         }
 
         // Create an empty ArrayList that we can start adding jokes to
-        ArrayList<String> jokes = new ArrayList<>();
+        val jokes = ArrayList<String>()
 
         // Try to parse the JSON response string. If there's a problem with the way the JSON
         // is formatted, a JSONException exception object will be thrown.
         // Catch the exception so the app doesn't crash, and print the error message to the logs.
         try {
             // Create a JSONObject from the JSON response string
-            JSONObject jsonObj = new JSONObject(jokeJSON);
+            val jsonObj = JSONObject(jokeJSON)
 
             // Getting JSON Array node
-            JSONArray valueArray = jsonObj.getJSONArray("value");
+            val valueArray = jsonObj.getJSONArray("value")
 
             // looping through All jokeArray
-            for (int i = 0; i < valueArray.length(); i++) {
-                JSONObject currentJoke = valueArray.getJSONObject(i);
-                String jokeString = currentJoke.getString("joke");
-                jokes.add(jokeString);
+            for (i in 0 until valueArray.length()) {
+                val currentJoke = valueArray.getJSONObject(i)
+                val jokeString = currentJoke.getString("joke")
+                jokes.add(jokeString)
             }
-            } catch(JSONException e){
-                // If an error is thrown when executing any of the above statements in the "try" block,
-                // catch the exception here, so the app doesn't crash. Print a log message
-                // with the message from the exception.
-                Log.e(LOG_TAG, "Problem parsing the joke JSON results", e);
-            }
+        } catch (e: JSONException) {
+            // If an error is thrown when executing any of the above statements in the "try" block,
+            // catch the exception here, so the app doesn't crash. Print a log message
+            // with the message from the exception.
+            Log.e(LOG_TAG, "Problem parsing the joke JSON results", e)
+        }
 
-            // Return the list of jokes
-            return jokes;
-
+        // Return the list of jokes
+        return jokes
     }
-
 }
