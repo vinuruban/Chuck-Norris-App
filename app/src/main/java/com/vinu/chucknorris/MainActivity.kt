528dfe56d0e667ff1dd705786b44737a3501bc99
Vinukshen <vinukshen@gmail.com>
2020-11-29 20:04:47 +0000

    Code cleaned + UI improved

--- app/src/main/java/com/vinu/chucknorris/MainActivity.kt
+++ app/src/main/java/com/vinu/chucknorris/MainActivity.kt
@@ -1,16 +1,12 @@
 package com.vinu.chucknorris
 
-import android.app.AlertDialog
-import android.content.DialogInterface
 import android.net.ConnectivityManager
 import android.os.AsyncTask
 import android.os.Bundle
-import android.view.Menu
-import android.view.MenuItem
 import android.view.View
+import android.view.inputmethod.InputMethodManager
 import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
-import kotlinx.android.synthetic.main.activity_main.*
 import java.util.*
 
 
@@ -25,9 +21,14 @@ class MainActivity : AppCompatActivity() {
     /** Adapter for the list of jokes */
     private var adapter: ArrayAdapter<String>? = null
 
+    /** Search for a specific joke */
     private var editTextBox: EditText? = null
 
-    /** URL to fetch 6 random jokes excluding the explicit ones */
+    /**
+     * 1) URL to fetch 6 random jokes excluding the explicit ones
+     * 2) URL to search for a specific joke
+     * Both URLs escapes specific characters
+     * */
     private val url = "http://api.icndb.com/jokes/"
     private val randomUrl = "random/6?exclude=[explicit]"
     private val escapeUrl = "escape=javascript"
@@ -37,17 +38,30 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        /** Setup attributes */
         val refreshButton = findViewById<View>(R.id.refreshButton) as Button
         val searchButton = findViewById<View>(R.id.searchButton) as Button
         editTextBox = findViewById<View>(R.id.editText) as EditText
 
+        /** Loads 6 random jokes */
         refreshJokeList(0)
 
+        /** When the refresh button is clicked */
         refreshButton.setOnClickListener {
+
+            /** Close keyboard */
+            closeKeyboard()
+
             refreshJokeList(0)
         }
 
+        /** When the search button is clicked */
         searchButton.setOnClickListener {
+
+            /** Close keyboard */
+            closeKeyboard()
+
+            /** if the edit text box is empty, display error message */
             if (editTextBox!!.text.isEmpty()) {
                 Toast.makeText(this, getString(R.string.enter_ID), Toast.LENGTH_SHORT).show();
             }
@@ -55,11 +69,18 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    /** To let keyboard disappear when clicked outside  */
+    fun closeKeyboard() {
+        val inputMethodManager = getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
+        inputMethodManager.hideSoftInputFromWindow(currentFocus!!.windowToken, 0)
+    }
+
     private fun refreshJokeList(digit: Int) {
 
+        /** Setup list view */
         listView = findViewById(R.id.listView)
 
-        /** Create a new adapter that takes an EMPTY list of jokes as input */
+        /** Setup the adapter */
         adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, jokeList)
 
         /** Set the adapter on the {@link ListView}
@@ -67,17 +88,18 @@ class MainActivity : AppCompatActivity() {
          */
         listView?.adapter = adapter
 
-        // Get a reference to the ConnectivityManager to check state of network connectivity
+        /** a reference to the ConnectivityManager to check the state of the network connectivity */
         val connMgr =
             getSystemService(CONNECTIVITY_SERVICE) as ConnectivityManager
-        // Get details on the currently active default data network
+        /** Get details on the currently active default data network */
         val networkInfo = connMgr.activeNetworkInfo
 
-        // If there is a network connection, fetch data
+        /** If there is a network connection, fetch data */
         if (networkInfo != null && networkInfo.isConnectedOrConnecting) {
             // Start the AsyncTask to fetch the data
             val task = JokeAsyncTask()
 
+            /** Url depends on the button that was clicked */
             if (digit == 0) {
                 task.execute("$url$randomUrl&$escapeUrl")
             } else {
@@ -90,35 +112,34 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    /** To perform the HTTP request for the data and process the response in the background thread */
     inner class JokeAsyncTask :
         AsyncTask<String?, Void?, ArrayList<String>?>() {
 
-        var mainActivity = MainActivity().callingActivity
-
+        /** Triggers methods in QueryUtils for JSON parsing */
         override fun doInBackground(vararg urls: String?): ArrayList<String>? {
-            // Don't perform the request if there are no URLs, or the first URL is null.
+            /** Don't perform the request if there are no URLs, or the first URL is null. */
             return if (urls.size < 1 || urls[0] == null) {
                 null
             } else urls[0]?.let { QueryUtils.fetchData(it) }
-
-            // Perform the HTTP request for the data and process the response.
         }
 
 
+        /** Post JSON Parsing */
         override fun onPostExecute(jokeObjects: ArrayList<String>?) {
 
-            // Clear the adapter of previous the data
+            /** Clear the adapter of previous the data */
             adapter?.clear()
 
             val button = findViewById<View>(R.id.refreshButton) as Button
 
-            // If there is a valid list of {@link Joke}s, then add them to the adapter's
-            // data set. This will trigger the ListView to update.
+            /** Update the list view with the joke responses */
             if (jokeObjects != null && !jokeObjects.isEmpty()) {
                 adapter?.addAll(jokeObjects)
                 button.text = getString(R.string.button_refresh)
             }
         }
+
     }
 
 }
--- app/src/main/java/com/vinu/chucknorris/QueryUtils.kt
+++ app/src/main/java/com/vinu/chucknorris/QueryUtils.kt
@@ -15,22 +15,25 @@ import java.nio.charset.Charset
 import java.util.*
 
 object QueryUtils {
-    /** Tag for the log messages  */
-    val LOG_TAG = QueryUtils::class.java.simpleName
 
+    /** Tag for the log messages  */
+    private val LOG_TAG = QueryUtils::class.java.simpleName
+
+    /** To extract data depending on request */
     private var searchActive = false
 
     /**
-     * Query the USGS dataset and return a list of [JokeObject] objects.
+     * Return a list of [ArrayList] objects.
      */
     fun fetchData(requestUrl: String): ArrayList<String>? {
 
+        /** Set searchActive to true if the Url doesn't contain "random" */
         searchActive = !requestUrl.contains("random")
 
-        // Create URL object
+        /** Create URL object */
         val url = createUrl(requestUrl)
 
-        // Perform HTTP request to the URL and receive a JSON response back
+        /** Perform HTTP request to the URL and receive a JSON response back */
         var jsonResponse: String? = null
         try {
             jsonResponse = makeHttpRequest(url)
@@ -38,9 +41,7 @@ object QueryUtils {
             Log.e(LOG_TAG, "Problem making the HTTP request.", e)
         }
 
-        // Extract relevant fields from the JSON response and create a list of {@link Joke}s
-
-        // Return the list of {@link Joke}s
+        /** Extract relevant fields from the JSON response */
         return extractFeatureFromJson(jsonResponse)
     }
 
@@ -64,7 +65,7 @@ object QueryUtils {
     private fun makeHttpRequest(url: URL?): String {
         var jsonResponse = ""
 
-        // If the URL is null, then return early.
+        /** If the URL is null, then return early. */
         if (url == null) {
             return jsonResponse
         }
@@ -77,8 +78,9 @@ object QueryUtils {
             urlConnection.requestMethod = "GET"
             urlConnection.connect()
 
-            // If the request was successful (response code 200),
-            // then read the input stream and parse the response.
+            /** If the request was successful (response code 200),
+             * then read the input stream and parse the response.
+             */
             if (urlConnection.responseCode == 200) {
                 inputStream = urlConnection.inputStream
                 jsonResponse = readFromStream(inputStream)
@@ -119,27 +121,30 @@ object QueryUtils {
      */
     fun extractFeatureFromJson(jokeJSON: String?): ArrayList<String>? {
 
-        // If the JSON string is empty or null, then return early.
+        /** If the JSON string is empty or null, then return early. */
         if (TextUtils.isEmpty(jokeJSON)) {
             return null
         }
 
-        // Create an empty ArrayList that we can start adding jokes to
+        /** An empty ArrayList to add the jokes in */
         val jokes = ArrayList<String>()
 
-        // Try to parse the JSON response string. If there's a problem with the way the JSON
-        // is formatted, a JSONException exception object will be thrown.
-        // Catch the exception so the app doesn't crash, and print the error message to the logs.
+        /** Try to parse the JSON response string. If there's a problem with the way the JSON
+         * is formatted, a JSONException exception object will be thrown.
+         * Catch the exception so the app doesn't crash, and print the error message to the logs.
+         */
         try {
-            // Create a JSONObject from the JSON response string
+            /** Create a JSONObject from the JSON response string */
             val jsonObj = JSONObject(jokeJSON)
 
+            /** If you're searching for a specific joke... */
             if(searchActive) {
-                // Getting JSON Array node
+
                 val valueObj = jsonObj.getJSONObject("value")
                 val jokeString = valueObj.getString("joke")
                 val categoriesArray = valueObj.getJSONArray("categories")
 
+                /** if the joke is explicit, do not show the joke */
                 if (categoriesArray.length() != 0) {
                     if (categoriesArray.get(0) == "explicit") {
                         jokes.add("Explicit jokes cannot be seen, please try another joke ID.")
@@ -150,10 +155,10 @@ object QueryUtils {
                 }
 
             } else {
-                // Getting JSON Array node
+
                 val valueArray = jsonObj.getJSONArray("value")
 
-                // looping through All jokeArray
+                /** Loop through all of value Array's elements */
                 for (i in 0 until valueArray.length()) {
                     val currentJoke = valueArray.getJSONObject(i)
                     val jokeString = currentJoke.getString("joke")
@@ -161,13 +166,15 @@ object QueryUtils {
                 }
             }
         } catch (e: JSONException) {
-            // If an error is thrown when executing any of the above statements in the "try" block,
-            // catch the exception here, so the app doesn't crash. Print a log message
-            // with the message from the exception.
+            /** If an error is thrown when executing any of the above statements in the "try" block,
+             * catch the exception here, so the app doesn't crash. Print a log message
+             * with the message from the exception.
+             */
+
             Log.e(LOG_TAG, "Problem parsing the joke JSON results", e)
         }
 
-        // Return the list of jokes
+        /** Return the list of jokes */
         return jokes
     }
 }
